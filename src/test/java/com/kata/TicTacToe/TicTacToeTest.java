3f0828ad8991713af93b2b419eb088badffd6678
Priyal Parikh <parikhpriyal91@yahoo.com>
2020-08-08 15:38:58 +0530

    Refactor: Move the TicTacToe initialization to Before method

--- src/test/java/com/kata/TicTacToe/TicTacToeTest.java
+++ src/test/java/com/kata/TicTacToe/TicTacToeTest.java
@@ -1,29 +1,35 @@
 package com.kata.TicTacToe;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 
 public class TicTacToeTest
 {
+    public static final String PLAYER_ONE_NAME = "X";
+    TicTacToe ticTacToe;
+
+    @Before
+    public void setUp()
+    {
+        ticTacToe=new TicTacToe();
+    }
 
     @Test
     public void createNewTicTacToeBoard()
     {
-        TicTacToe ticTacToe=new TicTacToe();
-
         assertNotNull(ticTacToe.getBoard());
     }
-
 
     @Test
     public void playerXmakesTheFirstMove()
     {
-        TicTacToe ticTacToe=new TicTacToe();
-
-        ticTacToe.makeaMove("X", 0, 0);
+        ticTacToe.makeaMove(PLAYER_ONE_NAME, 0, 0);
 
         assertEquals("X",ticTacToe.getBoard()[0][0]);
     }
+
 }
